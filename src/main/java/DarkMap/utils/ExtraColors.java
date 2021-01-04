2d84b5d970fe43da60b21e8c93ce8c79fed0d61f
casey <406326-casey@users.noreply.gitlab.com>
2021-01-03 22:41:00 -0500

    Tweaked colors a tad

--- src/main/java/DarkMap/utils/ExtraColors.java
+++ src/main/java/DarkMap/utils/ExtraColors.java
@@ -14,9 +14,12 @@ public class ExtraColors {
     public static final Color LEGEND_BG = new Color(0.278f, 0.231f, 0.251f, 0.0f);
     public static final Color LEGEND_MAIN_TEXT = new Color(0.741f, 0.784f, 0.851f, 1.0f);
 
-    public static final Color MAP_CIRCLE = new Color(0.588f, 0.596f, 0.773f, 1.0f);
+    //public static final Color MAP_CIRCLE = new Color(0.588f, 0.596f, 0.773f, 1.0f);
 
     //public static final Color MAP_EDGE_AVAILABLE = new Color(0.388f, 0.545f, 0.769f, 1.0f);
     //public static final Color MAP_EDGE_AVAILABLE = MAP_CIRCLE.cpy();
-    public static final Color MAP_EDGE_AVAILABLE = new Color(0.408f, 0.420f, 0.749f, 1.0f);
+    //public static final Color MAP_EDGE_AVAILABLE = new Color(0.408f, 0.420f, 0.749f, 1.0f);
+
+    public static final Color MAP_EDGE_AVAILABLE = new Color(0.631f, 0.631f, 0.620f, 1.0f);
+    public static final Color MAP_CIRCLE = MAP_EDGE_AVAILABLE.cpy();
 }
